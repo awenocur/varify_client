38d8a1399728ce9a6d4837b7c2953a66cbce8e03
Adam Wenocur <wenocur@email.chop.edu>
2014-06-17 15:58:05 -0400

    added download URL

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     maintainer='Adam Wenocour',
     maintainer_email='wenocur@email.chop.edu',
     description="an HTTP client and supporting utilities for the Varify API",
+    download_url='https://github.com/awenocur/varify_client.git',
     entry_points={
         "console_scripts": ['fetchVCF = varify_client._fetchVCF:runCommandLine']
     },
