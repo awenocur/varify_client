6c4718ac8b9e1f53c4942c01967dbf630b2b2642
Adam Wenocur <wenocur@email.chop.edu>
2014-06-17 15:24:47 -0400

    various changes for setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='varify-client',
+    name='varify_client',
     version='0.0a1',
-    packages='varify-client',
+    packages=['varify_client'],
     description="an HTTP client and supporting utilities for the Varify API",
     entry_points={
         "console_scripts": ['fetchVCF = varify_client._fetchVCF:runCommandLine']
     },
-    url='https://github.com/awenocur/varify_client'
+    url='https://github.com/awenocur/varify_client',
+    install_requires=["PyVCF"]
 )
--- varify_client/_fetchVCF.py
+++ varify_client/_fetchVCF.py
@@ -7,73 +7,69 @@ import json
 import re
 from util import chromRange
 
-class VariantVcfDownload:
-    @staticmethod
-    def fetchVcfPipe(sample_list, ranges=None, host=None, port=None, protocol=None, token=None):
-        tokenQueryString = ''
+def fetchVcfPipe(sample_list, ranges=None, host=None, port=None, protocol=None, token=None):
+    tokenQueryString = ''
 
-        if host == None:
-           host = connectionConfig.host
-        if port == None:
-           port = connectionConfig.port
-        if protocol == None:
-           protocol = connectionConfig.protocol
-        if token == None:
-           token = connectionConfig.token
+    if host == None:
+       host = connectionConfig.host
+    if port == None:
+       port = connectionConfig.port
+    if protocol == None:
+       protocol = connectionConfig.protocol
+    if token == None:
+       token = connectionConfig.token
 
-        if token != None:
-           tokenQueryString = '?token=' + token
+    if token != None:
+       tokenQueryString = '?token=' + token
 
-        for_export = {}
+    for_export = {}
 
-        if ranges != None:
-            rangesForExport = []
-            for range in ranges:
-                rangesForExport.append(range.dict())
-            for_export['ranges'] = rangesForExport
+    if ranges != None:
+        rangesForExport = []
+        for range in ranges:
+            rangesForExport.append(range.dict())
+        for_export['ranges'] = rangesForExport
 
-        for_export['samples'] = sample_list
+    for_export['samples'] = sample_list
 
-        data = json.dumps(for_export)
+    data = json.dumps(for_export)
 
-        request = urllib2.Request(protocol + "://" + host + ":" + str(port) + "/api/data/export/vcf/" + tokenQueryString, data=data, headers={'Content-type': 'application/json'})
-        return urllib2.urlopen(request)
+    request = urllib2.Request(protocol + "://" + host + ":" + str(port) + "/api/data/export/vcf/" + tokenQueryString, data=data, headers={'Content-type': 'application/json'})
+    return urllib2.urlopen(request)
 
-    @staticmethod
-    def fetchVcf(*args, **kwargs):
-        pipe = VariantVcfDownload.fetchVcfPipe(*args, **kwargs)
-        reader = vcf.Reader(fsock=pipe)
-        return reader
+def fetchVcf(*args, **kwargs):
+    pipe = VariantVcfDownload.fetchVcfPipe(*args, **kwargs)
+    reader = vcf.Reader(fsock=pipe)
+    return reader
 
-    @staticmethod
-    def runCommandLine():
-        #The following is a Python 2.7+ implementation; use optparse for Python 2.6
-        argParser = ArgumentParser("fetch VCF representation of variant data")
-        argParser.add_argument('--sample', '-s', dest='sampleNames', nargs='+', type=str)
-        argParser.add_argument('--range', '-r', dest='ranges', nargs='+', type=str)
-        args = argParser.parse_args()
+def runCommandLine():
+    #The following is a Python 2.7+ implementation; use optparse for Python 2.6
+    argParser = ArgumentParser("fetch VCF representation of variant data")
+    argParser.add_argument('--sample', '-s', dest='sampleNames', nargs='+', type=str)
+    argParser.add_argument('--range', '-r', dest='ranges', nargs='+', type=str)
+    args = argParser.parse_args()
 
-        if args.sampleNames == None:
-            print "no sample labels provided!"
-            return
+    if args.sampleNames == None:
+        print "no sample labels provided!"
+        return
 
-        ranges = []
-        rangeStrings = args.ranges
-        rangePattern = re.compile(r"(?P<chr>.*?):(?P<begin>.*?)-(?P<end>.*?)$")
-        if rangeStrings != None:
-            for string in rangeStrings:
-                rangeProps = rangePattern.match(string)
-                newRange = chromRange(
-                    rangeProps.group("chr"),
-                    int(rangeProps.group("begin")),
-                    int(rangeProps.group("end"))
-                )
-                ranges.append(newRange)
+    ranges = []
+    rangeStrings = args.ranges
+    rangePattern = re.compile(r"(?P<chr>.*?):(?P<begin>.*?)-(?P<end>.*?)$")
+    if rangeStrings != None:
+        for string in rangeStrings:
+            rangeProps = rangePattern.match(string)
+            newRange = chromRange(
+                rangeProps.group("chr"),
+                int(rangeProps.group("begin")),
+                int(rangeProps.group("end"))
+            )
+            ranges.append(newRange)
 
-        try:
-             vcfStream = VariantVcfDownload.fetchVcfPipe(args.sampleNames, ranges = ranges)
-             print vcfStream.read()[:-1]
-        except urllib2.HTTPError, e:
-             print "HTTP error: %d" % e.code
-        except urllib2.URLError, e:
-             print "Network error: %s" % e.reason.args[1]
+    try:
+         vcfStream = VariantVcfDownload.fetchVcfPipe(args.sampleNames, ranges = ranges)
+         print vcfStream.read()[:-1]
+    except urllib2.HTTPError, e:
+         print "HTTP error: %d" % e.code
+    except urllib2.URLError, e:
+         print "Network error: %s" % e.reason.args[1]
