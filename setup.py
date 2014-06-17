fa00699a7fdcf2c23f948e94ce79c45790a27c90
Adam Wenocur <wenocur@email.chop.edu>
2014-06-17 15:54:45 -0400

    added myself as an author

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='varify-client',
     version='0.0a1',
     packages=['varify_client'],
+    maintainer='Adam Wenocour',
+    maintainer_email='wenocur@email.chop.edu',
     description="an HTTP client and supporting utilities for the Varify API",
     entry_points={
         "console_scripts": ['fetchVCF = varify_client._fetchVCF:runCommandLine']
