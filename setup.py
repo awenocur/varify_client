0c7ab06971dfeeff6af2138cfc8ba25cbd1fecb3
Adam Wenocur <wenocur@email.chop.edu>
2014-06-17 15:48:08 -0400

    fixed internal package identifier

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='varify-client',
     version='0.0a1',
-    packages=['varify-client'],
+    packages=['varify_client'],
     description="an HTTP client and supporting utilities for the Varify API",
     entry_points={
         "console_scripts": ['fetchVCF = varify_client._fetchVCF:runCommandLine']
