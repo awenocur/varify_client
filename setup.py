eb12c2235ecdccb17463b4e325868ca55db6fd38
Adam Wenocur <wenocur@email.chop.edu>
2014-06-19 15:10:16 -0400

    bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='varify-client',
-    version='0.0.2',
+    version='0.0.3',
     packages=['varify_client'],
     maintainer='Adam Wenocour',
     maintainer_email='wenocur@email.chop.edu',
