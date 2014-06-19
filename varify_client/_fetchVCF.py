64c44fb21c94a61dd8166072a6c64d597a4c19a6
Adam Wenocur <wenocur@email.chop.edu>
2014-06-19 11:19:11 -0400

    added detection of range formatting errors

--- varify_client/_fetchVCF.py
+++ varify_client/_fetchVCF.py
@@ -6,21 +6,24 @@ from config import connectionConfig, updateConfigPath, readConfig
 import json
 import re
 from util import chromRange
+from sys import stderr
 
-def fetchVcfPipe(sample_list, ranges=None, host=None, port=None, protocol=None, token=None):
+
+def fetchVcfPipe(sample_list, ranges=None, host=None, port=None, protocol=None,
+                 token=None):
     tokenQueryString = ''
 
     if host == None:
-       host = connectionConfig.host
+        host = connectionConfig.host
     if port == None:
-       port = connectionConfig.port
+        port = connectionConfig.port
     if protocol == None:
-       protocol = connectionConfig.protocol
+        protocol = connectionConfig.protocol
     if token == None:
-       token = connectionConfig.token
+        token = connectionConfig.token
 
     if token != None:
-       tokenQueryString = '?token=' + token
+        tokenQueryString = '?token=' + token
 
     for_export = {}
 
@@ -34,13 +37,17 @@ def fetchVcfPipe(sample_list, ranges=None, host=None, port=None, protocol=None, 
 
     data = json.dumps(for_export)
 
-    request = urllib2.Request(protocol + "://" + host + ":" + str(port) + "/api/data/export/vcf/" + tokenQueryString, data=data, headers={'Content-type': 'application/json'})
+    request = urllib2.Request(protocol + "://" + host + ":" + str(
+        port) + "/api/data/export/vcf/" + tokenQueryString, data=data,
+                              headers={'Content-type': 'application/json'})
     return urllib2.urlopen(request)
+
 
 def fetchVcf(*args, **kwargs):
     pipe = fetchVcfPipe(*args, **kwargs)
     reader = vcf.Reader(fsock=pipe)
     return reader
+
 
 def runCommandLine():
     #The following is a Python 2.7+ implementation; use optparse for Python 2.6
@@ -60,21 +67,25 @@ def runCommandLine():
     if rangeStrings != None:
         for string in rangeStrings:
             rangeProps = rangePattern.match(string)
-            newRange = chromRange(
-                rangeProps.group("chr"),
-                int(rangeProps.group("begin")),
-                int(rangeProps.group("end"))
-            )
-            ranges.append(newRange)
+            try:
+                newRange = chromRange(
+                    rangeProps.group("chr"),
+                    int(rangeProps.group("begin")),
+                    int(rangeProps.group("end"))
+                )
+                ranges.append(newRange)
+            except AttributeError, e:
+                print >> stderr, "ignoring range \"" + string + "\": improperly formatted"
+
 
     if args.configPath != None:
         updateConfigPath(args.configPath)
     readConfig()
 
     try:
-         vcfStream = fetchVcfPipe(args.sampleNames, ranges = ranges)
-         print vcfStream.read()[:-1]
+        vcfStream = fetchVcfPipe(args.sampleNames, ranges=ranges)
+        print vcfStream.read()[:-1]
     except urllib2.HTTPError, e:
-         print "HTTP error: %d" % e.code
+        print >> stderr, "HTTP error: %d" % e.code
     except urllib2.URLError, e:
-         print "Network error: %s" % e.reason.args[1]
+        print >> stderr, "Network error: %s" % e.reason.args[1]
